ff6807686a2992437c621c49fc9fefba0cb8fb4c
brahimafa <brahimafassy@gmail.com>
2021-06-15 19:56:16 +0100

    chnaged app path from nginx location to a server block

--- src/routes/buld.js
+++ src/routes/buld.js
@@ -7,6 +7,16 @@ const { ConfigureDatabase, buildAndRunDockerImage, cloneRepoAndAddDockerFile, se
 const router = express.Router();
 const { logme } = require('../utils/logger');
 
+const defaultPort = (projectType, customPort) => {
+    switch (projectType) {
+    case 'core':
+        return 80;
+    case 'php':
+        return 80;
+    default:
+        return customPort;
+    }
+};
 // const body =
 router.post('/build', async (req, res) => {
     const { body } = req;
@@ -15,20 +25,38 @@ router.post('/build', async (req, res) => {
     if (!body.repository) return res.status(400).send('Repo Required');
     const splitedRepo = body.repository.split('/');
     [,,, repoInfo.user, repoInfo.repoName] = splitedRepo;
-    repoInfo.repoNameCleaned = repoInfo.repoName.replace(/-|.git/, '').toLocaleLowerCase();
+    repoInfo.repoName = repoInfo.repoName.replace('.git', '');
+    repoInfo.repoNameCleaned = repoInfo.repoName.replace(/-/gi, '').toLocaleLowerCase();
     repoInfo.user = repoInfo.user.replace(/-|_|@/, '').toLocaleLowerCase();
     const imageName = `${repoInfo.user}/${repoInfo.repoNameCleaned}`;
     const containerName = `${repoInfo.user}-${repoInfo.repoNameCleaned}`;
+    body.projectType = body.projectType.toLocaleLowerCase();
 
+    const customPort = defaultPort(body.projectType, body.customPort);
     console.log(repoInfo);
-    (async () => {
-        // await cloneRepoAndAddDockerFile({ ...body, folderName: containerName });
-        await ConfigureDatabase(body); // build BB
-        await buildAndRunDockerImage({ repositoryName: repoInfo.repoName, containerName, imageName });
-        await setUpNginxConfiguration({ containerName, customPort: body.customPort });
-    })();
+    // return res.send('done');
 
-    return res.send('done');
+    let result;
+    const messages = [];
+    let folderName = containerName;
+    const chnagedConfiguration = {}; // this variable holds information if changed in some step;
+    result = await cloneRepoAndAddDockerFile({ ...body, folderName }, chnagedConfiguration);
+    if (result.status !== 200) return res.status(400).json({ error: result.data });
+    messages.push(result.data);
+    if (chnagedConfiguration.folderName) folderName = chnagedConfiguration.folderName;
+    result = await ConfigureDatabase(body); // build BB
+    if (result.status !== 200) return res.status(400).json({ messages, error: result.data });
+    messages.push(result.data);
+
+    result = await buildAndRunDockerImage({ folderName, containerName, imageName });
+    if (result.status !== 200) return res.status(400).json({ messages, error: result.data });
+    messages.push(result.data);
+
+    result = await setUpNginxConfiguration({ containerName, customPort });
+    if (result.status !== 200) return res.status(400).json({ messages, error: result.data });
+    messages.push(result.data);
+
+    return res.json({ messages });
 });
 
 module.exports = router;
--- src/routes/config.js
+++ src/routes/config.js
@@ -1,40 +1,102 @@
 const { exec, writeFile, readFile, copyFile, randomPort, send } = require('../utils/functions');
 const { logme } = require('../utils/logger');
 
-function handleShellresult(result,step) {
+function handleShellresult(result, step) {
+    console.log(result);
     const { error, message } = result;
-    if(!error) return send(`[DONE] ${step} passed Succefully`);
+    // db ERRORS FROM 700 to 799
+    // container Errors From 600 to 699
+    // Git errors From 500 to 599
+    // ERROR 400 error not treated well (mazal madartch lih logic dyalo m had l error)
+    if (!error) return send(`[DONE] ${step} passed Succefully`);
     const isDBExist = message.includes('database exists');
-    if(isDBExist) return send('The Database Already Exist', 400);
-    return send(message,400);
+    if (isDBExist) return send('[ERROR] The Database Already Exist', 700);
+    const isContainerAlreadyUsed = message.includes('already in use');
+    if (isContainerAlreadyUsed) return send('[ERROR] This Container is Already Running', 600);
+    const isGitFolderAlreadyExist = message.includes('already exists and is not an empty directory');
+    if (isGitFolderAlreadyExist) return send('[ERROR] Git Folder [With] The SAME Name Already Exists', 500);
+
+    return send(`Error Was Incoutred in step : ${step}`, 400);
 }
 
-async function cloneRepoAndAddDockerFile({ repository, projectType, repositoryName,folderName }) {
-    logme('Clonning the repo');
+async function cloneRepoAndAddDockerFile({ repository, projectType, folderName, extra },chnagedConfiguration) {
+    const step = 'Clonning the repo';
+    logme(step);
     let result = await exec(`cd /repos; git clone ${repository} ${folderName}`); // FIXEME : add clone name to this!!
-    console.log(result);
+    let HandledResult = handleShellresult(result, step);
+    if (HandledResult.status !== 200) {
+        if (HandledResult.status === 500) {
+            result = await exec(`cd /repos/${folderName} ; git pull`);
+            HandledResult = handleShellresult(result, step);
+            if (HandledResult.status !== 200) return send(HandledResult.data, HandledResult.status);
+            // eslint-disable-next-line no-param-reassign
+            extra = 'Github Repository Already Exist, Only New Changes Were Pulled';
+        } else {
+            return send(HandledResult.data, HandledResult.status);
+        }
+    }
     try {
-        const type = projectType.toLocaleLowerCase();
         logme('adding docker file');
-        result = await copyFile(`${global.appRoot}/dockerfiles/Dockerfil-${type}`, `/repos/${repositoryName}/Dockerfile`);
-        console.log();
+        const type = projectType.toLocaleLowerCase();
+        if (type === 'core') {
+            result = await exec(`find /repos/${folderName} -name "*.sln"`);
+            if (!result.message) return send('[ERROR] There is No Solution file (.sln) in this repo ');
+            const solutionPathSplited = result.message.split('/');
+            solutionPathSplited.splice(solutionPathSplited.length - 1, 1);
+            solutionPathSplited.splice(0 ,2); // remove repos ['','rep']
+
+            const solutionFolder = solutionPathSplited.join('/');
+            console.log('woooooooooow', solutionFolder);
+            // eslint-disable-next-line no-param-reassign
+            folderName = solutionFolder;
+            chnagedConfiguration.folderName = solutionFolder;
+            if (HandledResult.status !== 200) return send(HandledResult.data, HandledResult.status);
+            result = await copyFile(`${global.appRoot}/dockerfiles/entrypoint-core.sh`, `/repos/${folderName}/entrypoint.sh`);
+        }
+        result = await copyFile(`${global.appRoot}/dockerfiles/Dockerfile-${type}`, `/repos/${folderName}/Dockerfile`);
+        console.log('[DONE] FILE COPY RESULT', result);
+
+        let message = '[DONE] Project Cloned and Dockerfile Added';
+        if (extra) message += ` [With] ${extra}`;
+        return send(message);
     } catch (error) {
         console.log('[ERROR] copy docker file', error);
+        return send('[ERROR] copy docker file', 400);
     }
 }
 
-async function buildAndRunDockerImage({ repositoryName, containerName, imageName }) {
-    logme('Building the Docker image');
-    let result = await exec(`cd /repos/${repositoryName}; docker build -t ${imageName} .`);
-    console.log(result);
-    logme('Running the docker image');
+async function buildAndRunDockerImage({ folderName, containerName, imageName, extra }) {
+    let step = 'Building the Docker image';
+    logme(step);
+    let result;
+    if (!extra) {
+        result = await exec(`cd /repos/${folderName}; docker build -t ${imageName} .`);
+        console.log(step, result);
+    }
+    step = 'Running the docker image';
+    logme(step);
     result = await exec(`docker run --name ${containerName} --rm -d --net ensas-net ${imageName}`);
-    console.log(result);
-
+    let HandledResult = handleShellresult(result, step);
+    let messageAfterReRunnung;
+    if (HandledResult.status !== 200) {
+        if (HandledResult.status === 600) {
+            step = 'Removing Docker Container';
+            result = await exec(`docker rm ${containerName} -f`);
+            HandledResult = handleShellresult(result, step);
+            console.log(step, HandledResult);
+            if (HandledResult.status !== 200) return send(HandledResult.data, HandledResult.status);
+            result = await buildAndRunDockerImage({ folderName, containerName, imageName, extra: `Container ${containerName} Was Running and Stopped` });
+            messageAfterReRunnung = result.data;
+        } else return send(HandledResult.data, HandledResult.status);
+    }
+    let message = messageAfterReRunnung || `[DONE] ${step}`;
+    if (extra) message += ` [With] ${extra}`;
+    return send(message, 200);
 }
 
 async function setUpNginxConfiguration({ containerName, customPort }) {
-    logme('Set Up Nginx Location');
+    let step = 'Set Up Nginx Location';
+    logme(step);
     const defaultNginxLoncationConf = await readFile(`${global.appRoot}/templates/nginx-location.conf`);
     // eslint-disable-next-line consistent-return
     const replacedNginxConf = defaultNginxLoncationConf.toString().replace(/(?<name>__NAME__)|(?<port>__PORT__)/gm, (...matche) => {
@@ -43,28 +105,68 @@ async function setUpNginxConfiguration({ containerName, customPort }) {
         if (groups.name) return containerName;
         if (groups.port) return customPort;
     });
-    await writeFile(`${global.appRoot}/nginx/config/${containerName}.ensas-conf`, replacedNginxConf, 'utf8');
-    logme('resload nginx ');
-    let result = await exec('docker exec nginx sh -c "nginx -t && nginx -s reload"');// if config is good reload service.
-    console.log(result);
+    await writeFile(`${global.appRoot}/nginx/config/${containerName}.conf`, replacedNginxConf, 'utf8');
+    step = 'Relaoding Nginx';
+    logme(step);
+    const result = await exec('docker exec nginx sh -c "nginx -t && nginx -s reload"');// if config is good reload service.
+    const HandledResult = handleShellresult(result, step);
+    if (HandledResult.status !== 200) return send(HandledResult.data, HandledResult.status);
+    return send('[DONE] Nginx Configuration Passed ^^');
+}
+
+async function ConfigureDatabaseMyql({ sqlFile, extra }) {
+    let HandledResult;
+    console.log(1);
+    let result;
+    const step = 'Configuring Mysql Database';
+    console.log(2);
+
+    result = await exec(`docker exec mysql2 sh -c "mysql -uroot -proot < /DB_SHARED_FILES/${sqlFile}"`);
+    HandledResult = handleShellresult(result, step);
+    let messageAfterDbError;
+    console.log(3);
+    if (HandledResult.status !== 200) {
+        console.log(4);
+
+        if (HandledResult.status === 700) {
+            console.log(5);
+
+            // needs -REVIEW-  this line gets the database name from the previous error by spliting.
+            const databaseName = result.message.split('create database')[1].split(';')[0].replace(/'|"/g, '');
+            console.log(6);
+
+            result = await exec(`docker exec mysql2 sh -c "mysqladmin -uroot -proot drop -f ${databaseName}"`);
+            console.log(6);
+
+            HandledResult = handleShellresult(result, step);
+            console.log(7);
+
+            if (HandledResult.status !== 200) return send(HandledResult.data, HandledResult.status);
+
+            console.log(8);
+            result = await ConfigureDatabaseMyql({ sqlFile, extra: `Database '${databaseName}' Was Droped` });
+            console.log('result', result);
+            messageAfterDbError = result.data;
+        } else return send(HandledResult.data, HandledResult.status);
+        console.log(9);
+    }
+    let message = messageAfterDbError || '[DONE] Database Imported';
+    if (extra) message += ` [With] : ${extra}`; // in cas if any changes are made after an error is happend
+    return send(message);
 }
 
 async function ConfigureDatabase({ dbType, sqlFile }) {
     // TODO - check if drop exist in this file!!
     logme(`Set up Database [${dbType}]`);
-    let result;
     switch (dbType) {
     case 'mysql2':
-        result = await exec(`docker exec ${dbType} sh -c "mysql -uroot -proot < /DB_SHARED_FILES/${sqlFile}"`);
-        return handleShellresult(result);
-    case 'MSSQL':
-        console.log('this Not Implimented Yet!!!');
-        break;
+        return ConfigureDatabaseMyql({ sqlFile });
+    case 'mssql':
+        return send('[DONE] MSSQL useses EF6 to Build database');
     case 'MONGODB':
-        console.log('this Not Implimented Yet!!!');
-        break;
+        return send('[ERROR] Not Implemented Yet', 400);
     default:
-        break;
+        return send(`[ERROR] ${dbType} Not Implemented Yet`, 400);
     }
 }
 
